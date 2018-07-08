1c8567329c736391dac3a5ae72dd429a3cbe9f03
Marko <koron.marko@gmail.com>
2018-07-08 22:15:45 +0200

    Installed Bloomer and Bulma. Created some base pads layout.

--- src/App.js
+++ src/App.js
@@ -1,27 +1,85 @@
 import React, { Component } from 'react';
+import 'bulma/css/bulma.min.css';
+import { Container, Columns, Column, Box, Title } from 'bloomer';
 
+
+
+const audios = [
+  {
+    id: 'Q',
+    src: 'sounds/clap-808.mp3',
+    text: 'clap-808'
+  },
+  {
+    id: 'W',
+    src: 'sounds/clap-707.mp3',
+    text: 'clap-707'
+  },
+  {
+    id: 'E',
+    src: 'sounds/clap-707.mp3',
+    text: 'clap-101'
+  },
+  {
+    id: 'Q',
+    src: 'sounds/clap-808.mp3',
+    text: 'clap-808'
+  },
+  {
+    id: 'W',
+    src: 'sounds/clap-707.mp3',
+    text: 'clap-707'
+  },
+  {
+    id: 'E',
+    src: 'sounds/clap-707.mp3',
+    text: 'clap-101'
+  },
+  {
+    id: 'Q',
+    src: 'sounds/clap-808.mp3',
+    text: 'clap-808'
+  },
+  {
+    id: 'W',
+    src: 'sounds/clap-707.mp3',
+    text: 'clap-707'
+  },
+  {
+    id: 'E',
+    src: 'sounds/clap-707.mp3',
+    text: 'clap-101'
+  }
+] 
 
 class App extends Component {
   render() {
     return (
-      <div className="App" id="drum-machine">
+      <Container className="App" id="drum-machine">
+        <Title hasTextAlign='centered'>Drum Machine</Title>
         <Display />
-      </div>
+      </Container>
     );
   }
 }
 
 const Display = () => (
   <div id="display">
-    <p>Hello World</p>
-    <DrumPad />
+    <Pads />
   </div>
 );
 
-const DrumPad = () => (
-  <div class="drum-pad">
-    <p>M</p>
-  </div>
+const DrumPad = (props) => (
+    <Column isSize="1/3">
+      <Box className="drum-pad" hasTextAlign='centered'>{props.sound.text}</Box>
+    </Column>
+);
+
+
+const Pads = () => (
+  <Columns isMultiline>
+    {audios.map( (item) => <DrumPad sound={item} />)}
+  </Columns>
 );
 
 export default App;
